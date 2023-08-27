3e52b84df27b9940aa0236eec80874daa4504fdf
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2023-08-27 01:43:01 -0300

    Criação da camada de serviço

---
+++ dslist/src/main/java/com/projetospring/dslist/services/GameService.java
@@ -0,0 +1,19 @@
+package com.projetospring.dslist.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.projetospring.dslist.entities.Game;
+import com.projetospring.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+	private GameRepository gameRepository;
+    
+    public List<Game> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result;
+	}
+}
